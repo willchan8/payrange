e740a621b8feb9c27c0ab69ddf48e49fb203254c
William Chan <wchan1992@gmail.com>
2020-06-21 21:19:31 -0700

    Render all components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import listItems from '../data/listItems';
 
 function Dashboard() {
-  const [washers] = useState(listItems.devices);
+  const [washers, setWashers] = useState(listItems.devices);
+  const [balance, setBalance] = useState(30);
 
   return (
     <div className="dashboard">
@@ -10,22 +11,30 @@ function Dashboard() {
         <div>Select a machine</div>
         <div className="add-item">+</div>
       </div>
-      {washers.map((washer) =>
-        <div className="container">
-          <div className="left-col">
-            <div className="washer-id">
-              <div>{washer.id}</div>
+      <div className="washer-list">
+        {washers.map((washer) =>
+          <div className="container">
+            <div className="left-col">
+              <div className="washer-id">
+                <div>{washer.id}</div>
+              </div>
+              <div className="text-col">
+                <div className="washer-text">{washer.label}</div>
+                <div className="available-text">{washer.status}</div>
+              </div>
             </div>
-            <div className="text-col">
-              <div className="washer-text">{washer.label}</div>
-              <div className="available-text">{washer.status}</div>
+            <div className="red-circle">
+              <div>&mdash;</div>
             </div>
           </div>
-          <div className="circle">
-            <div>&mdash;</div>
-          </div>
+        )}
+      </div>
+      <div className="balance-footer">
+        <div>Balance: ${balance.toFixed(2)}</div>
+        <div className="blue-circle">
+          +
         </div>
-      )}
+      </div>
     </div>
   );
 }
--- src/data/listItems.js
+++ src/data/listItems.js
@@ -3,6 +3,14 @@ export default {
   { "id": "1", "label": "Washer 1", "status": "Available" },
   { "id": "2", "label": "Washer 2", "status": "Available" },
   { "id": "3", "label": "Washer 3", "status": "Available" },
-  { "id": "4", "label": "Washer 4", "status": "Busy" }
+  { "id": "4", "label": "Washer 4", "status": "Busy" },
+  { "id": "1", "label": "Washer 1", "status": "Available" },
+  { "id": "2", "label": "Washer 2", "status": "Available" },
+  { "id": "3", "label": "Washer 3", "status": "Available" },
+  { "id": "4", "label": "Washer 4", "status": "Busy" },
+  { "id": "1", "label": "Washer 1", "status": "Available" },
+  { "id": "2", "label": "Washer 2", "status": "Available" },
+  { "id": "3", "label": "Washer 3", "status": "Available" },
+  { "id": "4", "label": "Washer 4", "status": "Busy" },
   ]
 }
