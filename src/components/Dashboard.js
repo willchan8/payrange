0304e382743fb47b2946ff9b1a99cbddf3193cc5
William Chan <wchan1992@gmail.com>
2020-06-22 14:30:54 -0700

    Complete Login and Dashboard using class components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,66 +1,72 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
+import { withRouter } from "react-router-dom";
 import listItems from '../data/listItems';
 
-const Dashboard = () => {
-  const [washers, setWashers] = useState(listItems.devices);
-  const [balance, setBalance] = useState(25);
-  const [currentId, setId] = useState(listItems.devices.length)
-
-  const mounted = useRef();
-  useEffect(() => {
-    if (!mounted.current) {
-      mounted.current = true;
-    } else {
-      setWashers(washers => [...washers, {"id": currentId, "label": `Washer ${currentId}`, "status": "Busy"}])
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      washers: listItems.devices,
+      balance: 25,
+      currentId: listItems.devices.length
     }
-  }, [currentId])
-
-  const handleAddWasher = () => {
-    setId(currentId => currentId + 1);
   }
 
-  const handleRemoveWasher = (id) => {
+  handleAddWasher = () => {
+    this.setState(prevState => ({ currentId: prevState.currentId + 1 }), 
+      () => {
+        const { washers, currentId } = this.state;
+        this.setState({ washers: [...washers, {"id": currentId, "label": `Washer ${currentId}`, "status": "Busy"}]});
+      }
+    );
+  }
+
+  handleRemoveWasher = (id) => {
+    const { washers } = this.state;
     const list = [...washers];
     const updatedList = list.filter(item => item.id !== id);
-    setWashers(updatedList);
+    this.setState({ washers: updatedList });
   }
   
-  const handleAddBalance = () => {
-    setBalance(balance => balance + 5)
+  handleAddBalance = () => {
+    this.setState(prevState => ({ balance: prevState.balance + 5 }));
   }
 
-  return (
-    <div className="dashboard">
-      <div className="header">
-        <div>Select a machine</div>
-        <div className="add-item" onClick={handleAddWasher}>+</div>
-      </div>
-      <div className="washer-list">
-        {washers.map((washer) =>
-          <div className="container" key={washer.id}>
-            <div className="left-col">
-              <div className="washer-id">
-                <div>{washer.id}</div>
+  render() {
+    const { washers, balance } = this.state;
+    return (
+      <div className="dashboard">
+        <div className="header">
+          <div>Select a machine</div>
+          <div className="add-item" onClick={this.handleAddWasher}>+</div>
+        </div>
+        <div className="washer-list">
+          {washers.map((washer) =>
+            <div className="container" key={washer.id}>
+              <div className="left-col">
+                <div className="washer-id">
+                  <div>{washer.id}</div>
+                </div>
+                <div className="text-col">
+                  <div className="washer-text">{washer.label}</div>
+                  <div className="available-text">{washer.status}</div>
+                </div>
               </div>
-              <div className="text-col">
-                <div className="washer-text">{washer.label}</div>
-                <div className="available-text">{washer.status}</div>
+              <div className="red-circle" onClick={() => this.handleRemoveWasher(washer.id)}>
+                <div>&mdash;</div>
               </div>
             </div>
-            <div className="red-circle" onClick={() => handleRemoveWasher(washer.id)}>
-              <div>&mdash;</div>
-            </div>
+          )}
+        </div>
+        <div className="balance-footer">
+          <div>Balance: ${balance.toFixed(2)}</div>
+          <div className="blue-circle" onClick={this.handleAddBalance}>
+            <div>+</div>
           </div>
-        )}
-      </div>
-      <div className="balance-footer">
-        <div>Balance: ${balance.toFixed(2)}</div>
-        <div className="blue-circle">
-          <div onClick={handleAddBalance}>+</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default Dashboard;
+export default withRouter(Dashboard);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,57 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
+import { withRouter } from "react-router-dom";
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  let history = useHistory();
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
 
-  const handleSubmit = (e) => {
+  handleSubmit = (e) => {
+    const { history } = this.props;
     e.preventDefault();
     history.push('/dashboard');
   }
 
-  return (
-    <div className="login-page">
-      <div className="header">
-        <div>Welcome</div>
+  handleChange = (e) => {
+    this.setState({ [e.target.type]: e.target.value });
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div className="login-page">
+        <div className="header">
+          <div>Welcome</div>
+        </div>
+        <div className="content">
+          <div className="login-text">Login</div>
+          <form className="center" onSubmit={this.handleSubmit}>
+            <input 
+              className="input-box"
+              type="email"
+              placeholder="Email" 
+              value={email}
+              onChange={this.handleChange}
+              required
+            />
+            <input 
+              className="input-box" 
+              type="password" 
+              placeholder="Password" 
+              value={password} 
+              onChange={this.handleChange}
+              required
+            />
+            <input className="login-button" type="submit" value="Login" />
+          </form>
+        </div>
       </div>
-      <div className="content">
-        <div className="login-text">Login</div>
-        <form className="center" onSubmit={handleSubmit}>
-          <input 
-            className="input-box"
-            type="email"
-            placeholder="Email" 
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            required
-          />
-          <input 
-            className="input-box" 
-            type="password" 
-            placeholder="Password" 
-            value={password} 
-            onChange={e => setPassword(e.target.value)}
-            required
-          />
-          <input className="login-button" type="submit" value="Login" />
-        </form>
-      </div>
-    </div>
-  );
+    );
+}
 }
 
-export default Login;
+export default withRouter(Login);
